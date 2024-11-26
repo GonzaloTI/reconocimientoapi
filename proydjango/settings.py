ad3a6004e28bb15c091f8bf498d668186ef6995d
GonzaloTI <ignaciogonzaloid@gmail.com>
2024-11-26 09:56:13 -0300

    aws

--- proydjango/settings.py
+++ proydjango/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = [
     'web-production-7b18.up.railway.app',
     'web-production-3654.up.railway.app',
     'web-production-dd2e9.up.railway.app',
+    '3.84.141.249',
     '127.0.0.1',
     '8080'
 ]
